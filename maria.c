c557529e911c70d3eedf006eb286db08b9d7e11d
Raspberry Pi <raspberrypisig@gmail.com>
2023-02-15 23:43:10 +1100

    need to get camera right

--- maria.c
+++ maria.c
@@ -40,8 +40,8 @@ int main(void)
 
     // Define the camera to look into our 3d world
     Camera camera = { 0 };
-    camera.position = (Vector3){ 0.0f, 20.0f, 17.0f }; 
-    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };    
+    camera.position = (Vector3){ 0.0f, 10.0f, 17.0f }; 
+    camera.target = (Vector3){ -10.0f, 20.0f, 0.0f };    
     //camera.position = (Vector3){ 20.0f, 30.0f, -60.0f }; // Camera position
     //camera.target = (Vector3){ 0.0f, 0.0f, 60.0f };     // Camera looking at point
     camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
